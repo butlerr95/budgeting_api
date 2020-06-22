d778525ba0b33692b4e88cd41b6dbf6be6ee00a6
butlerr95 <reece.butler@hotmail.co.uk>
2020-06-22 19:48:25 +0100

    Fix indentation issues

--- app.py
+++ app.py
@@ -8,26 +8,26 @@ from routes.expense import expense as expense_routes
 from routes.category import category as category_routes
 
 def create_app():
-''' Creates and returns the Flask app by loading config, 
-    registering blueprints and initialising the DB. '''
+	''' Creates and returns the Flask app by loading config, 
+    	registering blueprints and initialising the DB. '''
 
-    app = Flask(__name__)
-    app.config.from_object('config.Config')
+	app = Flask(__name__)
+	app.config.from_object('config.Config')
 
-    app.register_blueprint(expense_routes, url_prefix="/expense")
-    app.register_blueprint(category_routes, url_prefix="/category")
+	app.register_blueprint(expense_routes, url_prefix="/expense")
+	app.register_blueprint(category_routes, url_prefix="/category")
 
-    db.init_app(app)
+	db.init_app(app)
 
-    # Ensure FOREIGN KEY constraint for sqlite3
-    if 'sqlite' in app.config['SQLALCHEMY_DATABASE_URI']:
-        def _fk_pragma_on_connect(db, conn_record):  # noqa
-            db.execute('pragma foreign_keys=ON')
+	# Ensure FOREIGN KEY constraint for sqlite3
+	if 'sqlite' in app.config['SQLALCHEMY_DATABASE_URI']:
+		def _fk_pragma_on_connect(db, conn_record):  # noqa
+			db.execute('pragma foreign_keys=ON')
 
-        with app.app_context():
-            event.listen(db.engine, 'connect', _fk_pragma_on_connect)
-           
-            # Create database and tables if they don't exist
-            db.create_all()
-            
-    return app
+		with app.app_context():
+			event.listen(db.engine, 'connect', _fk_pragma_on_connect)
+
+			# Create database and tables if they don't exist
+			db.create_all()
+
+	return app
