8cf7ba85e3f611a16f512bbd437c978eb928ba12
butlerr95 <reece.butler@hotmail.co.uk>
2020-07-11 17:14:53 +0100

    Updated monthly summary to calculate cum. total

--- routes/monthly.py
+++ routes/monthly.py
@@ -2,6 +2,7 @@
 
 import json
 import datetime
+import math
 
 from flask import Blueprint, Response, request
 
@@ -21,31 +22,38 @@ def get_monthly_summary():
     # Get week_start param and calculate the week_end date
     query_string_dict = request.args
     month_start = query_string_dict["month_start"]
-    # TODO: Calculate month end from month start!
-    month_end = query_string_dict["month_end"]
 
     # Get all dates in month
     current_date = datetime.datetime.strptime(month_start, r"%Y-%m-%d")
-    month_end_date = datetime.datetime.strptime(month_end, r"%Y-%m-%d")
+    month_end_date = datetime.datetime(current_date.year + (current_date.month == 12), 
+            (current_date.month + 1 if current_date.month < 12 else 1), 1) - datetime.timedelta(days=1)
+    
+    month_end = month_end_date.strftime(r"%Y-%m-%d")
 
     dates = dict()
-
-    while current_date <= month_end_date:
-        dates[current_date.strftime(r"%Y-%m-%d")] = 0
-        current_date = current_date + datetime.timedelta(days=1)
 
     result = db.session.query(Expense.date, func.sum(Expense.amount).label("total"))\
                     .filter(Expense.date >= month_start, Expense.date <= month_end)\
                     .group_by(Expense.date)\
                     .all()
 
-    cumulative_total = 0
-
-    # TODO: Cumulative total of all days, not just the ones that have expenditure data!
+    result_dict = dict()
 
     for r in result:
-        cumulative_total += r[1]
-        dates[r[0]] = cumulative_total
+        result_dict[r[0]] = r[1]
+
+    cumulative_total = 0
+
+    # Loop through all dates in month and update cumulative total from results
+    while current_date <= month_end_date:
+        date = current_date.strftime(r"%Y-%m-%d")
+
+        if date in result_dict.keys():
+            cumulative_total += result_dict[date]
+            
+        dates[current_date.strftime(r"%Y-%m-%d")] = math.floor(cumulative_total / 0.01) * 0.01
+
+        current_date = current_date + datetime.timedelta(days=1)
 
     json_response = json.dumps(dates)
 
